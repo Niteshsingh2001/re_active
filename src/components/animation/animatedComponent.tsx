dcd875bff97ee6a86e258e05f7eeda1df7c15885
Nitesh Singh <ni30singh420@gmail.com>
2024-03-19 09:58:22 +0530

    added animations

--- src/components/animation/animatedComponent.tsx
+++ src/components/animation/animatedComponent.tsx
@@ -1,9 +1,164 @@
 
 
-export default function Pop() {
+
+export function Transforms() {
     return (
-        <div className='h-36 w-36'>
-            adsasd
+        <div className='h-36 w-36 bg-red-400 flex rounded-md items-center justify-center
+            transformers
+        
+        '>
+            Reactive
         </div>
     )
 }
+
+export function Transition() {
+    return (
+        <div className='h-36 w-36 bg-red-400 flex rounded-md items-center justify-center
+        transition
+        '>
+            Reactive
+        </div>
+    )
+}
+export function KeyFrames() {
+    return (
+        <div className='h-36 w-36 bg-red-400 flex rounded-md items-center justify-center
+        anim
+        '>
+            Reactive
+        </div>
+    )
+}
+
+export function Equilizer() {
+    return (
+        <div className='h-36 w-36 flex gap-2 bg-red-200 rounded-md items-end justify-center 
+         py-2
+        '>
+            <div className="bar1  rounded-t-sm bg-blue-500"></div>
+            <div className="bar2  rounded-t-sm bg-blue-500"></div>
+            <div className="bar3 rounded-t-sm bg-blue-500"></div>
+            <div className="bar4  rounded-t-sm bg-blue-500"></div>
+        </div>
+    )
+}
+export function Equilizer2() {
+    return (
+        <div className='h-36 w-36 flex gap-2 bg-red-200 rounded-md items-center justify-center 
+         py-2
+        '>
+            <div className="bar1  rounded-sm bg-blue-500"></div>
+            <div className="bar2  rounded-sm bg-blue-500"></div>
+            <div className="bar3  rounded-sm bg-blue-500"></div>
+            <div className="bar4  rounded-sm bg-blue-500"></div>
+        </div>
+    )
+}
+export function Equilizer3() {
+    return (
+        <div className='h-36 w-36 flex gap-2 bg-red-200 rounded-md items-start justify-center 
+         py-2
+        '>
+            <div className="bar1  rounded-sm bg-blue-500"></div>
+            <div className="bar2  rounded-sm bg-blue-500"></div>
+            <div className="bar3  rounded-sm bg-blue-500"></div>
+            <div className="bar4  rounded-sm bg-blue-500"></div>
+        </div>
+    )
+}
+export function Pop() {
+    return (
+        <div className='h-36 w-36 flex gap-2 bg-red-200 rounded-md items-center justify-center 
+         py-2
+        '>
+            <div className="pop h-20 w-20 rounded-full bg-red-600"></div>
+        </div>
+    )
+}
+export function Heart() {
+    return (
+        <div className='h-36 w-36 flex gap-2  rounded-md items-center justify-center 
+         py-2 text-5xl 
+        '>
+            <div className="heart select-none">
+                💗
+            </div>
+        </div>
+    )
+}
+export function PopUp() {
+    return (
+        <div className='h-36 w-36 flex gap-2  rounded-md items-center justify-center 
+         py-2 text-5xl 
+        '>
+            Happy Birthday
+        </div>
+    )
+}
+export function Prespective() {
+    return (
+        <div className='h-36 w-36 flex gap-2  rounded-md 
+          bg-green-300 overflow-hidden  text-7xl items-center justify-center
+          dimension 
+       '>
+            😇
+            {/* <div className="bg-red-500 h-full w-full  dimension">
+                Nitesh Singh
+            </div>
+            <div>
+                Menu
+            </div> */}
+        </div>
+    )
+}
+
+export function Wheel() {
+    return (
+        <div className='h-36 w-36 flex gap-1 flex-row  rounded-md 
+          bg-black  overflow-hidden  items-center justify-center
+       '>
+            <div className="w-fit h-fit text-3xl wheel" >🛞</div>
+            <div className="w-fit h-fit text-3xl wheel" >🛞</div>
+            <div className="w-fit h-fit text-3xl wheel" >🛞</div>
+            {/* <div className="bg-red-500 h-full w-full  dimension">
+                Nitesh Singh
+            </div>
+            <div>
+                Menu
+            </div> */}
+        </div>
+    )
+}
+export function Bounce() {
+    return (
+        <div className='h-36 w-36 flex gap-2  rounded-md 
+          bg-violet-500  overflow-hidden  items-center justify-center
+       '>
+            <button className="bounce">Bounce</button>
+        </div>
+    )
+}
+export function Theme() {
+    return (
+        <div className="h-36 w-36 flex rounded-md 
+            overflow-hidden items-center justify-center theme border-2 border-black">
+            <div className="text-8xl rotate">
+            </div>
+
+        </div>
+    )
+}
+export function MoonFaces() {
+    return (
+        <div className="h-36 w-36 flex rounded-md 
+            overflow-hidden items-center justify-center theme border-2 border-black">
+            <div className="text-8xl rotate">
+            </div>
+
+        </div>
+    )
+}
+
+
+
--- src/components/animation/index.tsx
+++ src/components/animation/index.tsx
@@ -1,11 +1,24 @@
+import { Bounce, Equilizer, Equilizer2, Equilizer3, Heart, MoonFaces, Pop, Prespective, Theme, Wheel } from "./animatedComponent"
 import "./animation.css"
 
 export default function Animation() {
     return (
-        <div>
-            <div>
-                adsd
-            </div>
+        <div className="h-full w-full flex flex-wrap gap-4 select-none ">
+
+            {/* <Transforms /> */}
+            {/* <Transition /> */}
+            {/* <KeyFrames /> */}
+            <Equilizer />
+            <Equilizer2 />
+            <Equilizer3 />
+            <Pop />
+            <Heart />
+            <Prespective />
+            <Wheel />
+            <Bounce />
+            <Theme />
+            <MoonFaces />
+
 
         </div>
     )
